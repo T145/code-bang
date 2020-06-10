a8d05bc09a62eb13cbff90c1113cdb87be8ab04a
T145 <T145@protonmail.com>
2020-06-09 21:02:14 -0400

    Better decision logic

--- codebang.py
+++ codebang.py
@@ -1,29 +1,37 @@
 import requests
 from bs4 import BeautifulSoup as Soup
-import secrets
+from random import choices
 import sys
 sys.dont_write_bytecode = True
 
-def get_wikipedia_langs():
-    resp = requests.get('https://en.wikipedia.org/wiki/List_of_programming_languages')
+
+# TODO: Add check for count being <= 25
+def get_benfrederickson_langs(count):
+    resp = requests.get(
+        'https://www.benfrederickson.com/ranking-programming-languages-by-github-users/')
     page = Soup(resp.text, features='html.parser')
-    headings = page.body('div', attrs={'class': 'div-col columns column-width'})
-    langs = list()
+    table = page.find('table').find('tbody')('tr')
+    listings = [row('td')[1].text.strip() for row in table]
+    return choices(listings, k=count)
 
-    # There will likely be some language sanitization required, like w/ Viper for example
 
-    for heading in headings:
-        for child in heading('li'):
-            langs.append(child.a.text)
-    
-    return langs
+def get_wikipedia_langs(count):
+    resp = requests.get(
+        'https://en.wikipedia.org/wiki/List_of_programming_languages')
+    page = Soup(resp.text, features='html.parser')
+    listings = list()
+
+    for section in page.body('div', attrs={'class': 'div-col columns column-width'}):
+        for listing in section('li'):
+            listings.append(listing.a.text.strip())
+
+    return choices(listings, k=count)
+
 
 def main(av, ac):
-    langs = get_wikipedia_langs()
-    results = [secrets.choice(langs) for i in range(5)]
+    for i, lang in enumerate(get_wikipedia_langs(5)):
+        print('{}. {}'.format(i+1, lang))
 
-    for i, result in enumerate(results):
-        print('{}. {}'.format(i+1, result))
 
 if __name__ == "__main__":
     main(sys.argv, len(sys.argv))
